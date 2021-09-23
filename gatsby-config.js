dcadcbd63a548452879f63b8cd492001f05c66d6
Jacob Stephens <53905376+JacobStephens2@users.noreply.github.com>
2021-09-23 18:02:16 -0400

    update readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-notifications`,
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
---
+++ src/pages/to-do.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {Helmet} from "react-helmet";
+
+const ToDo = () => (
+    <div>
+		<Helmet>
+		<script src="https://p.trellocdn.com/embed.min.js" />
+		</Helmet>
+		<blockquote class="trello-card">
+			<a href="https:&#x2F;&#x2F;trello.com&#x2F;c&#x2F;C69BHk9b&#x2F;1-dana-alan-campaign-site">Dana Alan Campaign Site</a>
+		</blockquote>
+	</div>
+)
+
+export default ToDo;
